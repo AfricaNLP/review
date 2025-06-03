a37f885c82f5023f2d5c6217e752c2aa7564c62c
abumafrim <abumafrim@gmail.com>
2025-06-03 13:27:00 +0200

    updates

--- scripts/fetch.py
+++ scripts/fetch.py
@@ -1,5 +1,5 @@
 from fetcher import ArxivFetcher
 
 topics = ["machine translation", "sentiment analysis", "hate speech", "named entity recognition"]
-fetcher = ArxivFetcher(topics=topics, output_dir="content")
+fetcher = ArxivFetcher(topics=topics)
 fetcher.fetch_and_save()
